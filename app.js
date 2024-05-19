765869a46b49e4336133100deff9341b5dacdc82
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-05-19 21:14:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// Comparision Operators//
+var num1 = 10;
+var num2 = 15;
+console.log(num1 == num2);
+var num3 = "apple";
+var num4 = "apple";
+console.log(num3 == num4);
+var num5 = 10;
+var num6 = 9;
+console.log(num5 > num6);
+var num7 = 10;
+var num8 = 9;
+console.log(num7 != num8);
+var num01 = 23;
+var num02 = 14;
+console.log(num01 == num02);
+var num03 = 89;
+var num04 = 90;
+console.log(num03 > num04);
+var num9 = 45;
+var num10 = 34;
+console.log(num9 != num10);
---
+++ app.ts
@@ -0,0 +1,50 @@
+   // Comparision Operators  //
+
+let num1:number = 10;
+let num2:number = 15;
+console.log(
+    num1 == num2
+)                     // false
+
+let num3:String = "apple";
+let num4:String = "apple";
+
+console.log(
+    num3 == num4
+);        // true
+
+
+let num5:number = 10
+let num6:number = 9
+
+console.log(
+    num5 > num6
+);          //true
+
+
+let num7:number = 10
+let num8:number = 9
+
+console.log(
+    num7 != num8
+);     //true
+
+let num01:number = 23;
+let num02:number = 14;
+console.log(
+    num01 == num02
+);             // false
+
+
+let num03:number = 89;
+let num04:number = 90;
+
+console.log(
+    num03 !> num04
+);           //false
+
+let num9:number = 45
+let num10:number = 34
+console.log(
+    num9 != num10
+);     // true
